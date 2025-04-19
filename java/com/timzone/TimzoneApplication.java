1e9c974963fcf8c77dae607e95318cf34ec8c6e0
Timi Busayo <atimilewis55@gmail.com>
2025-04-19 09:29:16 +0100

    Initial commit of Tim-zone project

---
+++ java/com/timzone/TimzoneApplication.java
@@ -0,0 +1,13 @@
+package com.timzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimzoneApplication.class, args);
+	}
+
+}
